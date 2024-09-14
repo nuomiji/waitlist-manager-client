681b0e45424b3aa9354b7c32f6bc8e7799d37699
Kelly Dang <marksman@students.cs.ubc.ca>
2024-09-13 23:47:55 -0700

    Get customerId and position from server response and render them on page

--- src/Waitlist.tsx
+++ src/Waitlist.tsx
@@ -4,21 +4,33 @@ import axios from 'axios';
 function Waitlist() {
     const [name, setName] = useState('');
     const [partySize, setPartySize] = useState(1);
+    const [customerId, setCustomerId] = useState<number | null>(null);
+    const [position, setPosition] = useState<number | null>(null);
 
     async function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
-        console.log(`We are submitting! Name: ${name}, Party size: ${partySize}`);
 
         try {
-            const response = await axios.post('http://localhost:3001/api/customers', { name, partySize });
-            if (response.data) {
-                console.log(`Got a response from server! We are customer #${response.data.id}`);
+            const res = await axios.post('http://localhost:3001/api/customers', { name, partySize });
+            if (res.data) {
+                setCustomerId(res.data.id);
+                localStorage.setItem('customerId', res.data.id); // currently we lose customerId on page reload. Need to implement useEffect next
+                setPosition(res.data.position);
             } else {
                 console.error(`Did not get data back from server! No customer # was fetched`);
             }
         } catch (err) {
             console.error(`Failed to call server`);
         }
+    }
+
+    if (customerId) {
+        return (
+            <div>
+                <h2>You are customer number {customerId}</h2>
+                <p>There are {position} parties ahead of you in the queue.</p>
+            </div>
+        )
     }
 
     return (
