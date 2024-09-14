c64816571a85afd37039e71ee5b941fcb6ba75b1
Kelly Dang <marksman@students.cs.ubc.ca>
2024-09-14 01:49:23 -0700

    Add socket to emit setCustomerId

--- src/Waitlist.tsx
+++ src/Waitlist.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { io } from 'socket.io-client';
 
 // todo: make better types for these
 const NOT_QUEUED = 'notQueued';
 const WAITING = 'waiting';
 const TABLE_READY = 'tableReady';
 const SEATED = 'seated';
+
+let socket: any;
 
 function Waitlist() {
     const [name, setName] = useState('');
@@ -14,15 +17,45 @@ function Waitlist() {
     const [position, setPosition] = useState<number | null>(null);
     const [status, setStatus] = useState<string>(NOT_QUEUED);
 
-    // this is needed so we fetch from sessionStorage on page refresh
-    // at this time, we don't have websockets so we need to refresh the page
     useEffect(() => {
+        // initializes the Socket.io connection once when the component mounts, meaning once on page load
+        socket = io('http://localhost:3001');
+
+        // fetch from sessionStorage on page refresh
         const storedCustomerId = sessionStorage.getItem('customerId');
         if (storedCustomerId) {
             setCustomerId(Number(storedCustomerId));
             fetchCustomerDetails(Number(storedCustomerId));
+
+            console.log(`setting customreId ${storedCustomerId} on socket reconnect`)
+            socket.emit('setCustomerId', { customerId: storedCustomerId });
         }
+
+        // clean up socket connection when the component unmounts
+        return () => {
+            if (socket) {
+                socket.disconnect();
+            }
+        };
     }, []);
+
+    useEffect(() => {
+        if (socket && customerId) {
+            // this is not implemented in backend yet
+            socket.on('tableReady', (data: { customerId: number }) => {
+                console.log('Table is ready! Table is ready!')
+                if (data.customerId === customerId) {
+                    setStatus(TABLE_READY);
+                }
+            })
+        }
+
+        return () => {
+            if (socket) {
+                socket.off('tableReady');
+            }
+        }
+    }, [customerId]);
 
     if (status === SEATED) {
         return (
@@ -85,6 +118,8 @@ function Waitlist() {
                 sessionStorage.setItem('customerId', res.data.id); // currently we lose customerId on page reload. Need to implement useEffect next
                 setPosition(res.data.position);
                 setStatus('waiting');
+                console.log(`emitting customerIdMapping with customerId ${res.data.id}`);
+                socket.emit('setCustomerId', { customerId: res.data.id });
             } else {
                 console.error(`Did not get data back from server! No customer # was fetched`);
             }
