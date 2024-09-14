3fb5b318d6520b3d14ef36b2c096f43cc998f8fc
Kelly Dang <marksman@students.cs.ubc.ca>
2024-09-13 23:29:27 -0700

    Add form submit for addition to waitlist

--- src/Waitlist.tsx
+++ src/Waitlist.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function Waitlist() {
     const [name, setName] = useState('');
@@ -7,6 +8,17 @@ function Waitlist() {
     async function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
         console.log(`We are submitting! Name: ${name}, Party size: ${partySize}`);
+
+        try {
+            const response = await axios.post('http://localhost:3001/api/customers', { name, partySize });
+            if (response.data) {
+                console.log(`Got a response from server! We are customer #${response.data.id}`);
+            } else {
+                console.error(`Did not get data back from server! No customer # was fetched`);
+            }
+        } catch (err) {
+            console.error(`Failed to call server`);
+        }
     }
 
     return (
